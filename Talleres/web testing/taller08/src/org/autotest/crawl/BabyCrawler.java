0dedce9949f03593bf9f4425d362efdec4651ef3
nacho <ignaciolebrero@gmail.com>
2018-11-06 21:59:51 -0300

    listo taller de web

--- Talleres/web testing/taller08/src/org/autotest/crawl/BabyCrawler.java
+++ Talleres/web testing/taller08/src/org/autotest/crawl/BabyCrawler.java
@@ -38,15 +38,20 @@ public class BabyCrawler {
 	    while(!nextLinks.empty()) {
 	        String currentUrl = nextLinks.pop();
             visitedLinks.add(currentUrl);
-            logger.logVisitingUrl(currentUrl);
+
+            // Descomentar para ver las url que se van navegando
+            //logger.logVisitingUrl(currentUrl);
 
             try {
                 Set<String> newLinks = navigate(currentUrl);
                 updateWebLinks(website, currentUrl, newLinks);
                 int newLinksAmount = updateNextLinks(nextLinks, newLinks, visitedLinks, website.getDomainUrl());
+
+                // Descomentar para ver cuantos links se agregaron
                 //logger.logNumberOfLinks(currentUrl, newLinks.size(), newLinksAmount);
             } catch (HttpStatusException e) {
-                website.addHttpStatusException(e.getStatusCode(), e.getUrl());
+                // Cuando hay acentos la excepcion encodea el caracter, se lo revierte
+                website.addHttpStatusException(e.getStatusCode(), e.getUrl().replace("%C3%B3", "ó"));
             } catch (MalformedURLException e) {
                 website.addMalformedUrl(e.getMessage());
             } catch (UnsupportedMimeTypeException e) {
@@ -55,6 +60,7 @@ public class BabyCrawler {
                 website.addIOException(e, currentUrl);
             }
 
+            // Descomentar para ver estadisticas de lo que se va navegando.
             //logger.logStatistics(visitedLinks.size(), nextLinks.size());
         }
 
@@ -66,11 +72,7 @@ public class BabyCrawler {
     private int updateNextLinks(Stack<String> nextLinks, Set<String> newLinks, Set<String> visited, String domainUrl) {
         int linksamount = 0;
 
-
         for (String newLink : newLinks) {
-            if (newLink.equals("https://icc.fcen.uba.ar/wp-content/uploads/2018/06/vinculaci%C3%B3n_tsu_icc.jpg")) {
-                System.out.println("viniste de: ");
-            }
 
             if (newLink.startsWith(domainUrl) && !visited.contains(newLink) && !nextLinks.contains(newLink)) {
                 nextLinks.add(newLink);
@@ -83,10 +85,6 @@ public class BabyCrawler {
 
     private void updateWebLinks(WebSite website, String currentUrl, Set<String> links) {
         for (String link : links) {
-            if (link.equals("https://icc.fcen.uba.ar/wp-content/uploads/2018/06/vinculaci%C3%B3n_tsu_icc.jpg")) {
-                System.out.println("viniste de: " + link);
-            }
-
             if (link.startsWith(website.getDomainUrl())) {
                 website.addInternalLink(currentUrl, link);
             } else {
@@ -101,10 +99,12 @@ public class BabyCrawler {
         String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64)";
         USER_AGENT += " AppleWebKit/535.1 (KHTML, like Gecko)";
         USER_AGENT += " Chrome/13.0.782.112 Safari/535.1";
+
         try {
             Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
             Document htmlDocument = connection.get();
             Elements linksOnPage = htmlDocument.select("a[href]");
+
             for (Element elem : linksOnPage) {
                 links.add(elem.absUrl("href"));
             }
